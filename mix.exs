ed17c5389a9d11482cca993411bc2243567b1987
MauricioMSoares <mauricio.soares@sptech.school>
2023-10-30 11:34:14 -0300

    configured application to run supervisor when it starts

--- mix.exs
+++ mix.exs
@@ -4,17 +4,20 @@ defmodule Servy.Mixfile do
   def project do
     [
       app: :servy,
+      description: "A humble HTTP server",
       version: "0.1.0",
       elixir: "~> 1.13.4",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :eex]
+      extra_applications: [:logger, :eex],
+      mod: {Servy, []},
+      env: [port: 3000]
     ]
   end
 
